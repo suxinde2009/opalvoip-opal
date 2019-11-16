5736945996c42c3ff96d29f440c51516747dfa18
Robert Jongbloed <robertj@voxlucida.com.au>
2019-11-16 14:53:40 +0000

    Changed OPAL server star bash script to add to LD_LIBRARY_PATH instead of overwriting it.

--- samples/server/start.sh
+++ samples/server/start.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-export LD_LIBRARY_PATH=/usr/local/lib
+export LD_LIBRARY_PATH=/usr/local/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
 cd /opt/opalsrv/bin
 ulimit -c unlimited
 ./opalsrv --ini-file /opt/opalsrv/etc/opalsrv.ini --console --daemon
